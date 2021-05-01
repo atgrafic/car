fd6bb653fbd47f52d67a998793c22494a7e4c5f7
atgrafic <troczynska.anna@gmail.com>
2021-05-01 18:04:52 +0200

    back, localstorage

--- src/form.js
+++ src/form.js
@@ -1,3 +1,6 @@
+import style from "./css/index.scss";
+
+//alert("Hello! I am an alert box!!");
 const json = require("./json/list.json");
 const jsonList = json.carsData;
 const carName = document.getElementsByClassName("car");
@@ -14,45 +17,48 @@ function carNameClick() {
 function nextPage(e, id) {
     let carID = "";
     for (let i = 0; i < carName.length; i++) {
-     // console.log(id); //
-      //console.log('carName:', id  ); //
-     // console.log(carID);
-    //  console.log('jsonList', jsonList[i].id)
         if (id === jsonList[i].id) {
-            //    console.log(carName[i].id)
-            carID += `<h2 class="car-name">${jsonList[i].name}</h2>`;
+            carID += `<h2 class="car-name">${jsonList[i].name}</h2>
+            <div class="all_car_photo">
+            <img class="car-photo_1" src="${jsonList[i].image[0]}">
+            <img class="car-photo_2" src="${jsonList[i].image[1]}">
+            <img class="car-photo_3" src="${jsonList[i].image[2]}">
+            <img class="car-photo_4" src="${jsonList[i].image[3]}">
+            </div>
+            <div>
+            <button class="car-price">${jsonList[i].price}</button>
+            <p class="car-brand"> ${jsonList[i].brand}</p>
+            <p class="car-year">${jsonList[i].year}</p>
+            <p class="car-km">${jsonList[i].km}</p>
+            </div>
+            <div>
+            <h2>Opis</h2>
+            <p class="car-description">${jsonList[i].description}</p>
+            </div>
+            `;
 
-            //console.log(e);
-            // (B) SAVE TO LOCAL STORAGE
-            // localStorage.setItem("KEY", "VALUE");
-
-            //localStorage.setItem("carID", carID);
-            // local storage cannot store array and objects
-            // JSON encode before storing, convert to string
-            //localStorage.setItem("id", JSON.stringify(carID));
         }
     }
+    localStorage.setItem("carID", JSON.stringify(carID));
     location.href = nextURL;
-    console.log(id);
-    console.log(carID);
+    // console.log(id);
+    // console.log(carID);
+
 }
 
 carNameClick();
 
 function get() {
-    // (A) GET FROM SESSION
-    //let first = localStorage.getItem("first");
-    // JSON parse to turn stored sting back into array
-    let first = JSON.parse(localStorage.getItem("id"));
+      let carID = JSON.parse(localStorage.getItem("carID"));
 
-    // (B) IT WORKS!
-    // NOTE: Local storage is persistent
-    // Will not be deleted unless user clears browser data or manually cleared
-    console.log(first); // Foo Bar
-
-    document.getElementById("carSelected").innerHTML = localStorage.getItem("first");
-    // (EXTRA) TO CLEAR
-    // localStorage.removeItem("KEY");
-    // localStorage.clear();
+    console.log("localstorge:", carID);
+    document.getElementById("carSelected").innerHTML = carID;
 }
 get();
+
+//powrot
+function goBack() {
+    window.history.go(-1);
+  }
+
+  document.getElementById("back").addEventListener("click", goBack);
